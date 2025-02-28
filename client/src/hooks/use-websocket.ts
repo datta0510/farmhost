8a180b1b3cba1ab2074c00e0bf60a17f2163e198
datta0510 <demodatta0510@gmail.com>
2025-02-28 14:09:46 +0530

    Delete client directory

--- client/src/hooks/use-websocket.ts
+++
@@ -1,7 +0,0 @@
-// This file is kept as a placeholder but functionality is removed
-export function useWebSocket() {
-  return {
-    sendMessage: () => {},
-    isConnected: false
-  };
-}
--- client/src/main.tsx
+++
@@ -1,6 +0,0 @@
-import 'regenerator-runtime/runtime';
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
-
-createRoot(document.getElementById("root")!).render(<App />);
